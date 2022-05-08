c221be7ee589b27c5f1b4776dfa3e6a4674784b1
Rutuja Ambade <rutuja.ambade23@gmail.com>
2022-05-08 22:45:26 +0530

    Initial Commit

---
+++ DAY 11/FibSeries.java
@@ -0,0 +1,21 @@
+//fibonacci series
+
+import java.util.*;
+class FibSeries
+{
+public static void main(String arr[])
+{
+Scanner sc=new Scanner(System.in);
+int n1=0,n2=1,sum=0,i,lst;
+System.out.println("Enter How many number you want?: ");
+lst=sc.nextInt();
+
+for(i=2;i<=lst;i++)
+{
+sum=n1+n2;
+n1=n2;
+n2=sum;
+System.out.println(sum+" ");
+}
+}
+}
