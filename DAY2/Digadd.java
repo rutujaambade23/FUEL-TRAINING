7d7258a9dcf92a782a6bebfed34363d13ac3a971
Rutuja Ambade <rutuja.ambade23@gmail.com>
2022-05-08 22:16:01 +0530

    Initial Commit

---
+++ DAY2/Digadd.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Digadd
+{
+public static void main(String arr[])
+{
+int a,b,c,d;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter Number : ");
+a=sc.nextInt();
+b=a%10;
+a=a/10;
+c=a%10;
+d=c+b;
+
+System.out.println("Addition is: "+d);
+
+System.out.println("Substraction is: "+d);
+
+
+System.out.println("Multiplication is: "+d);
+
+
+System.out.println("Division is: "+d);
+
+
+
+}
+}
