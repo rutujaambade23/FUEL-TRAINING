25c24b94fd79dbffd96c7045c92e86e25a0c73ed
Rutuja Ambade <rutuja.ambade23@gmail.com>
2022-05-18 19:14:51 +0530

    Initial Commit

---
+++ DAY36/JPAwithWEBAPPLICATION/demohttpmethods/src/main/java/com/example/demohttpmethods/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.example.demohttpmethods;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//import com.example.crudapp.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Integer>{
+
+}
