f4b6d08e28b07fcbdea8881b1ef4069c91b57665
Rutuja Ambade <rutuja.ambade23@gmail.com>
2022-05-19 18:49:22 +0530

    Initial Commit

---
+++ DAY31/first/app/src/main/java/first/StudInterface.java
@@ -0,0 +1,12 @@
+package first;
+
+public class StudInterface {
+    public interface StudInterface {
+
+        void save(Studuent st);
+        void update(Studuent st);
+        void delete(Student st);
+        StudImpl getStud(int id);
+    }
+    
+}
