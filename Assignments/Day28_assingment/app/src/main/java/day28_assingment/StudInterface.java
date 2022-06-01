b3e45af42d51bdb22528a6a8cc84799de97064b3
Rutuja Ambade <rutuja.ambade23@gmail.com>
2022-06-01 12:38:25 +0530

    initial commit

---
+++ Assignments/Day28_assingment/app/src/main/java/day28_assingment/StudInterface.java
@@ -0,0 +1,17 @@
+package day28_assingment;
+
+public interface StudInterface {
+
+    void save(Student st);
+
+    void update(Student st);
+
+    void delete(Student st);
+
+    void insert(Student st);
+
+    void select(Student st);
+
+    Student getStud(int id);
+
+}
---
+++ day29Assignment/app/src/main/java/day29_assignment/StudInterface.java
@@ -0,0 +1,19 @@
+package day29_assignment;
+
+public interface StudInterface {
+
+    void save(Student st);
+
+    void update(Student st);
+
+    void delete(Student st);
+
+    void insert(Student st);
+
+    void select(Student st);
+
+    void create(Student st);
+
+    Student getStud(int id);
+
+}
